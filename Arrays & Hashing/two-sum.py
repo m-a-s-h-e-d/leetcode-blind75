f13984e4c1853c85b7b6bed880a8a18a43024a1f
Mashed <mattari.dev@gmail.com>
2024-11-20 03:32:09 -0800

    refactor: change to 2 space tab

--- Arrays & Hashing/two-sum.py
+++ Arrays & Hashing/two-sum.py
@@ -4,15 +4,15 @@ from collections import defaultdict
 from typing import List
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seenNum = defaultdict(int)
-        solution = []
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    seenNum = defaultdict(int)
+    solution = []
 
-        for i in range(len(nums)):
-            compliment = target - nums[i]
-            if (compliment in seenNum):
-                solution = [seenNum[compliment], i]
-                return solution
-            seenNum[nums[i]] = i
-        
+    for i in range(len(nums)):
+      compliment = target - nums[i]
+      if (compliment in seenNum):
+        solution = [seenNum[compliment], i]
         return solution
+      seenNum[nums[i]] = i
+    
+    return solution
