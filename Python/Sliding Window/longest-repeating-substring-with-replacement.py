f14820f9205b2389fae4dcd46cb012734e0e0436
Mashed <mattari.dev@gmail.com>
2024-11-26 04:22:06 -0800

    feat: complete 424. Longest Repeating Character Replacement

---
+++ Python/Sliding Window/longest-repeating-substring-with-replacement.py
@@ -0,0 +1,17 @@
+class Solution:
+  def characterReplacement(self, s: str, k: int) -> int:
+    count = {}
+    currentMax = 0
+    
+    left = 0
+    maxfreq = 0
+    for right in range(len(s)):
+      count[s[right]] = 1 + count.get(s[right], 0)
+      maxfreq = max(maxfreq, count[s[right]])
+
+      while (right - left + 1) - maxfreq > k:
+        count[s[left]] -= 1
+        left += 1
+      currentMax = max(currentMax, right - left + 1)
+
+    return currentMax
