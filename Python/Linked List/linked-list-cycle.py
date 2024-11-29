ae801b4d8af05f805e2779345617e96df40d6ef0
Mashed <mattari.dev@gmail.com>
2024-11-29 05:56:10 -0800

    refactor: change tab spacing to 2

--- Python/Linked List/linked-list-cycle.py
+++ Python/Linked List/linked-list-cycle.py
@@ -3,22 +3,22 @@
 from typing import Optional
 
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
 
 class Solution:
-    def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow = head
-        fast = head
+  def hasCycle(self, head: Optional[ListNode]) -> bool:
+    slow = head
+    fast = head
 
-        # With a slow and fast pointer, the fast pointer will have
-        # finished two iterations once the slow pointer finishes the first
-        # In the case of a cycle
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                return True
+    # With a slow and fast pointer, the fast pointer will have
+    # finished two iterations once the slow pointer finishes the first
+    # In the case of a cycle
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+      if slow == fast:
+        return True
 
-        return False
+    return False
--- Python/Linked List/remove-nth-node-from-end-of-list.py
+++ Python/Linked List/remove-nth-node-from-end-of-list.py
@@ -3,32 +3,32 @@
 from typing import Optional
 
 class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
 
 class Solution:
-    def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        prev = None
-        slow = head
-        fast = head
+  def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+    prev = None
+    slow = head
+    fast = head
 
-        # Constraints 1 <= n <= sz, so you don't need to check if the index fits
-        
-        # Move fast ahead by n nodes so the difference between fast and slow is n
-        for i in range(0, n):
-            fast = fast.next
+    # Constraints 1 <= n <= sz, so you don't need to check if the index fits
+    
+    # Move fast ahead by n nodes so the difference between fast and slow is n
+    for _ in range(0, n):
+      fast = fast.next
 
-        # Move fast to the end of the list
-        while fast:
-            prev = slow
-            slow = slow.next
-            fast = fast.next
+    # Move fast to the end of the list
+    while fast:
+      prev = slow
+      slow = slow.next
+      fast = fast.next
 
-        # Remove slow by setting previous to slow.next
-        if slow == head:
-            head = head.next
-        else:
-            prev.next = slow.next
+    # Remove slow by setting previous to slow.next
+    if slow == head:
+      head = head.next
+    else:
+      prev.next = slow.next
 
-        return head
+    return head
